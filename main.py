92835dd9274a30903ec02ba238ddd165ba418f04
Shan Jiang <sjiang@tmd.ca>
2019-07-18 16:06:40 -0400

    enable sending msg to multiple people

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import itchat
 import threading
 
 groupNames = ['group chat name 1', 'group chat name 2', 'group chat name 3', 'group chat name n']
-message = '// Python WeChat auto group chat test (time interval: 5s)'
+message = '// Python WeChat auto group chat test (time interval: 10s)'
 
 # Multiple groups
 def groupChat(groupNames, message):
@@ -19,9 +19,18 @@ def groupChat(groupNames, message):
             else:
                 print('No group found')
 
+nickNames = ['user 1', 'user 2', 'user 3', 'user n']
+
+def individualChat(userNames, message): 
+    for nickName in nickNames:
+        friends = itchat.search_friends(nickName=nickName)
+        for friend in friends:
+            itchat.send_msg(message, friend['UserName'])
+
 def main():
-    threading.Timer(5.0, main).start()
-    groupChat(groupNames, message)
+    threading.Timer(10.0, main).start()
+    # groupChat(groupNames, message)
+    individualChat(nickNames, message)
 
 itchat.auto_login(hotReload=True)
 main()
