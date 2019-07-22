a61a1fb58a2a8528ca141485c58c8341e6dadd97
Shan Jiang <sjiang@tmd.ca>
2019-07-22 09:03:42 -0400

    add comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import itchat
 import threading
 
-groupNames = ['group chat 1', 'group chat 2', 'group chat 3', 'group chat n']
+groupNames = ['group chat 1', 'group chat 2', 'group chat 3', 'group chat n'] # change the array items to target group chat names
 message = '// Python WeChat auto chat test (time interval: 10s)'
 
 # group chat
@@ -19,7 +19,7 @@ def groupChat(groupNames, message):
             else:
                 print('No group found')
 
-nickNames = ['user 1', 'user 2', 'user 3', 'user n']
+nickNames = ['user 1', 'user 2', 'user 3', 'user n'] # change the array items to target user names
 
 # individual chat
 def individualChat(userNames, message): 
@@ -31,7 +31,7 @@ def individualChat(userNames, message):
 def main():
     threading.Timer(10.0, main).start()
     # groupChat(groupNames, message)
-    #individualChat(nickNames, message)
+    # individualChat(nickNames, message)
 
 itchat.auto_login(hotReload=True)
 main()
